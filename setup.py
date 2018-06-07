f0f990c2a0d303e94a035558fbfae998d0f03cb2
Alex Marder <amarder89@gmail.com>
2018-06-07 13:09:23 -0400

    fixed setup.py

--- setup.py
+++ setup.py
@@ -5,33 +5,21 @@ import numpy as np
 
 from Cython.Build import cythonize
 
-# setup(
-#     name='bdrmapit',
-#     ext_modules=cythonize([
-#         'utils/utils.pyx',
-#         'utils/*.py',
-#         'bgp/routing_table.pyx',
-#         'bgp/bgp.pyx',
-#         'graph/bdrmapit.pyx',
-#         'graph/router.pyx',
-#         'graph/interface.pyx',
-#         'graph/hybrid_graph.pyx',
-#         'as2org.pyx',
-#         'updates_dict.pyx',
-#         'traceroute/abstract_parser.py',
-#         'traceroute/parser_pool_sqlite.py',
-#         'create_objs_sqlite.pyx',
-#         # 'algorithm_cy.pyx'
-#     ], nthreads=0)
-#     # ext_modules=cythonize(['ip2as.py'])
-# )
 setup(
     name='bdrmapit',
-    ext_modules=cythonize(
-        [
-            'traceroutecy/hop.pyx',
-            'bgp/routing_table.pyx'
-        ]
-    ),
-    include_dirs=[np.get_include()]
+    ext_modules=cythonize([
+        'utils/utils.pyx',
+        'utils/progress.py',
+        'bgp/routing_table.pyx',
+        'bgp/bgp.pyx',
+        'graph/bdrmapit.pyx',
+        'graph/router.pyx',
+        'graph/interface.pyx',
+        'graph/hybrid_graph.pyx',
+        'as2org.pyx',
+        'updates_dict.pyx',
+        'create_objs.pyx',
+        'traceroute/hop.pyx'
+    ], nthreads=0)
+    # ext_modules=cythonize(['ip2as.py'])
 )
