f340bf460511ad25ae7e874569c3a47a2df26aef
Alex Marder <amarder89@gmail.com>
2018-07-19 10:13:00 -0400

    Fixed nodes output and documentation.

--- nodes_as.py
+++ nodes_as.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python
 import sqlite3
-import sys
-from argparse import ArgumentParser, FileType
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 
 from utils.progress import Progress
 from utils.utils import File2
 
 
 def main():
-    parser = ArgumentParser()
-    parser.add_argument('-o', '--output', default='-')
-    parser.add_argument('-v', '--verbose', action='store_true')
-    parser.add_argument('db')
+    parser = ArgumentParser(description='Creates CAIDA nodes file from bdrmapIT sqlite output.', formatter_class=ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-o', '--output', default='-', help='Output file (- for stdout). Filenames ending in .bz2/gz will be compressed as bzip2/gzip.')
+    parser.add_argument('-v', '--verbose', action='store_true', help='Show progress in millions of database rows.')
+    parser.add_argument('db', help='The sqlite database file output by bdrmapIT.')
     args = parser.parse_args()
     query = 'SELECT nid, asn FROM node WHERE asn > -1 AND nid LIKE "N%"'
     con = sqlite3.connect(args.db)
